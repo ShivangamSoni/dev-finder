7d21281a3cf7ca65599f39315586e1faea86b800
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-10 12:45:18 +0530

    Add: Light Theme Colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: "class",
     theme: {
         extend: {
             colors: {
                 blue: "hsl(211, 100%, 50%)",
                 darkBlue300: "hsl(224, 40%, 20%)",
                 darkBlue500: "hsl(222, 40%, 13%)",
+                lightBlue50: "hsl(222, 100%, 98%)",
+                lightBlue500: "hsl(215, 33%, 45%)",
                 grey: "hsl(224, 11%, 60%)",
             },
             fontFamily: {
